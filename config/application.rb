536b3926afe142dd172cd91260b6e6e4a4d8b13a
GabeStah <gwyattkelsey@gmail.com>
2014-09-13 23:40:28 -0700

    Added precompile info to application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
+require 'sidekiq/api'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -13,6 +14,8 @@ Bundler.require(*Rails.groups)
 
 module KScraper
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.encoding = 'utf-8'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,5 +27,6 @@ module KScraper
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end

4b639f800c8c2088edbd97db10b5cf60d458bf4b
GabeStah <gwyattkelsey@gmail.com>
2014-09-16 02:38:01 -0700

    Fixed issue with passed page_count being set to last_occurrence timestamp

--- app/workers/scrape_worker.rb
+++ app/workers/scrape_worker.rb
@@ -8,6 +8,7 @@ class ScrapeWorker
   end
 
   def perform(page_count)
-    Post.populate_posts(page_count ? page_count : 1)
+    page_count = 1 if page_count && page_count.to_i > 50
+    Post.populate_posts(page_count)
   end
 end

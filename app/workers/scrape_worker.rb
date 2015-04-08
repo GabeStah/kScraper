fe1e0f87cbf1a700d662ec62ea5073056a27c612
GabeStah <gwyattkelsey@gmail.com>
2015-04-08 13:39:25 -0700

    Update scrape_worker.rb
    Fixed issue with extreme `page_count` or `thread_limit` values passed to `ScrapeWorker` jobs.

--- app/workers/scrape_worker.rb
+++ app/workers/scrape_worker.rb
@@ -8,8 +8,8 @@ class ScrapeWorker
   end
 
   def perform(page_count, thread_limit)
-    page_count = 1 if page_count && page_count.to_i > 50
-    thread_limit = 7 if thread_limit && thread_limit.to_i > 100
+    page_count = 1 if !page_count || (page_count && (page_count.to_i > 50 || page_count.to_i < 0))
+    thread_limit = 7 if !thread_limit || (thread_limit && (thread_limit.to_i > 100 || thread_limit.to_i < 0))
     Post.populate_posts(page_count, thread_limit)
   end
 end
